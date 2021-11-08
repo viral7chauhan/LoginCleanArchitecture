118649e0bddb795e5f9113424d5d9e276b318e6c
vchauhan <vchauhan@willowpump.com>
2021-11-08 14:25:54 +0530

    Initial Commit

---
+++ Shared/LoginCleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginCleanArchitectureApp.swift
+//  Shared
+//
+//  Created by Viral on 08/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LoginCleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
